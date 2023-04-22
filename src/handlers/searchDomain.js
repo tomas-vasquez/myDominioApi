4b9ae722f1843685e10cf875450fb5f1dd8c2623
Tomas Vasquez <tomasdetloging@gmail.com>
2023-04-22 00:36:50 -0400

    test commit

--- src/handlers/searchDomain.js
+++ src/handlers/searchDomain.js
@@ -1,5 +1,17 @@
-var checkDomainName = () => {
-  return "funciona papu";
+const domainCheck = require("domain-check");
+const Domain = domainCheck.Domain;
+
+var checkDomainName = async (name, res) => {
+  const domain = new Domain(name);
+
+  console.log("ooo:" + name);
+  // var result = await domain.isFree();
+  // console.log("pse");
+
+  setTimeout(() => {
+    res.send({ availableToRegister: Math.random() < 0.8 });
+    res.end();
+  }, 1000);
 };
 
-export { checkDomainName };
+module.exports = { checkDomainName };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 3001;
 
 const cors = require("cors");
+const morgan = require("morgan");
 
+const { checkDomainName } = require("./handlers/searchDomain");
+
+app.use(morgan("dev"));
 app.use(
   cors({
     origin: "*",
@@ -12,6 +16,15 @@ app.use(
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/check_domain", (req, res) => {
+  var domainName = req.query.name;
+  // setTimeout(() => {
+  checkDomainName(domainName, res);
+  // var response = checkDomainName(domainName);
+  // res.send(response);
+  // }, );r
 });
 
 app.listen(port, () => {
